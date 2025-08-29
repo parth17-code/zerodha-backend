895fee3fcf1d2300fe01ef69ae086dc22e0c78ed
parth17-code <2024.parth.mohite@ves.ac.in>
2025-08-29 11:50:23 +0530

    body-parser and cors added to index.js

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { HoldingsModel } = require("./models/HoldingsModel");
 const {PositionsModel} = require('./models/PositionsModel');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT;
 const url = process.env.MONGO_URL;
+
+app.use(cors());
+app.use(bodyParser.json())
+
+app.get('/allHoldings' , async(req,res)=>{
+    let allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings)
+})
+
+app.get('/allPositions' , async(req,res)=>{
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions)
+})
 
 app.listen(PORT || 3003, () => {
   console.log(`App started....listening on PORT ${PORT}`);
